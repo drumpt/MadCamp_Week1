06681e1c16136f79127fc9634eea476cb4efb972
Johyebin <40785543+Johyebin@users.noreply.github.com>
2020-01-07 00:10:53 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
@@ -62,9 +61,9 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                //int pos = tab.getPosition();
-                //checkView(pos);
-                //changeView(pos);
+                int pos = tab.getPosition();
+                checkView(pos);
+                changeView(pos);
             }
 
             @Override
@@ -90,15 +89,15 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case 1:
                 viewPager.setCurrentItem(1);
-                //checkView(index);
+                checkView(index);
                 break;
             case 2:
                 viewPager.setCurrentItem(2);
                 break;
         }
     }
-    //private boolean flag = false; // 아직 선택되지 않은 초기상태이면
-    /*private void  checkView(int index) {
+    private boolean flag = false; // 아직 선택되지 않은 초기상태이면
+    private void  checkView(int index) {
         if (index == 1) {
             if (flag == false) {
                 showSelect(); // 이미지 선택하는 액티비티 띄우기
@@ -106,7 +105,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-    */
 
         @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -128,17 +126,104 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-//        public void showSelect(){
-//            if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
-//                    != PackageManager.PERMISSION_GRANTED) {
-//                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
-//                return;
-//            }
-//            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-//            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-//            intent.setType("image/*");
-//            startActivityForResult(intent, 1);
-//        }
+        public void showSelect(){
+            if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+                return;
+            }
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+            intent.setType("image/*");
+            startActivityForResult(intent, 1);
+        }
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode == 1 && resultCode == RESULT_OK) {
+//            lstImg.add((ImageView)findViewById(R.id.img1));
+//            lstImg.add((ImageView)findViewById(R.id.img2));
+//            lstImg.add((ImageView)findViewById(R.id.img3));
+//            lstImg.add((ImageView)findViewById(R.id.img4));
+//            lstImg.add((ImageView)findViewById(R.id.img5));
+//            lstImg.add((ImageView)findViewById(R.id.img6));
+//            lstImg.add((ImageView)findViewById(R.id.img7));
+//            lstImg.add((ImageView)findViewById(R.id.img8));
+//            lstImg.add((ImageView)findViewById(R.id.img9));
+//            lstImg.add((ImageView)findViewById(R.id.img10));
+//            lstImg.add((ImageView)findViewById(R.id.img11));
+//            lstImg.add((ImageView)findViewById(R.id.img12));
+                ClipData clipData = data.getClipData();
+                if (clipData != null) { // 이미지가 여러장 선택되었을 경우
+                    for (int i = 0; i < clipData.getItemCount(); i++) {
+                        Uri imageUri = clipData.getItemAt(i).getUri();
+                        uris.add(imageUri);
+                        try {
+                            InputStream is = getContentResolver().openInputStream(imageUri);
+                            Bitmap bitmap = BitmapFactory.decodeStream(is);
+                            bitmaps.add(bitmap);
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } else { // 이미지가 한 장만 선택되었을 경우
+                    Uri imageUri = data.getData();
+                    uris.add(imageUri);
+                    try {
+                        InputStream is = getContentResolver().openInputStream(imageUri);
+                        Bitmap bitmap = BitmapFactory.decodeStream(is);
+                        bitmaps.add(bitmap);
+
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+         /*   new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (final Bitmap b : bitmaps) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                imageView.setImageBitmap(b);
+                            }
+                        });
+                        try {
+                            Thread.sleep(2000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
+          */
+          /*  GridLayout layout = (GridLayout) findViewById(R.id.parent_layout);
+            LayoutParams layoutParams = new LayoutParams(GridLayout.spec(150),GridLayout.spec(150));
+            for (int i = 0; i < bitmaps.size(); i++) {
+                ImageView iv = new ImageView(this);// 새로 추가할 imageView 생성
+                iv.setImageBitmap(bitmaps.get(i)); // imageView에 내용 추
+                iv.setLayoutParams(layoutParams);  // imageView layout 설정
+                lstImg.add(iv);
+                layout.addView(iv); // 기존 linearLayout에 imageView 추가
+            }
+            */
+
+            }
+
+            Log.d("@@@@@TAG@@@@","MainActivity1 called");
+            bundle = new Bundle();
+           tmpList = new ArrayList<String>();
+           for(Uri uri:uris) {
+              tmpList.add(uri.toString());
+          }
+            bundle.putStringArrayList("result_images",tmpList);
+            fragmentGallery.setArguments(bundle);
+            adapter.notifyDataSetChanged();
+            tabLayout.getTabAt(0).setIcon(R.drawable.address_book);
+            tabLayout.getTabAt(1).setIcon(R.drawable.gallery);
+            tabLayout.getTabAt(2).setIcon(R.drawable.restaurant_tab);
+//            myPhotoAdapter = new RecyclerViewAdapterImg(this);
+        }
 //            fragmentGallery.setAr(uris);
 }
-
